98cc4befbf9d080084ed1be5cb91e30128c055aa
Sean Murray <smurra42@jhu.edu>
2021-03-05 12:00:18 -0500

    added a bit to h files

---
+++ Cache.h
@@ -0,0 +1,52 @@
+// Cache.h
+
+#ifndef _CACHE_H
+#define _CACHE_H
+
+// Includes go here:
+#include "Set.h"
+#include <vector>
+
+// Class to represent a Cache
+class Cache {
+		// Statistics
+		long loads;	// Total loads
+		long stores;	// Total stores
+		long lHits;	// Load hits
+		long lMisses;	// Load misses
+		long sHits;	// Store hits
+		long sMisses;	// Store misses
+		long cycles;	// Total cycles
+
+		// Cache parameters:
+		// Number of sets per cache
+		// Number of blocks per set
+		// Number of bytes per block
+		
+		// Vector of sets
+		std::vector<Set> sets;			
+		// Parameters for specific behavior 
+		bool write-allocate;	// If false, then no-write-allocate
+		bool write-through;	// If false, then write-back
+		bool lru;		// If false, then fifo
+
+	// Public functions
+	public:
+		// Constructor
+		Cache(int sets, int blocks, int bytes, bool writeAl, bool writeTh, bool lru);
+		// Destructor (not sure if needed, but maybe for vectors)
+		~Cache();		
+
+		// Probably have a load function (what will parameter be)
+		void load(uint32_t address);
+		// Probably have a store function
+		void store(uint32_t address);
+
+		// Print statistics
+		void printSummary();
+
+	// Helper functions
+	private:
+};
+
+#endif
---
+++ Set.h
@@ -0,0 +1,27 @@
+// Set.h
+
+#ifndef _SET_H
+#define _SET_H
+
+// Includes go here:
+#include <vector>
+
+// Class to represent a set in a Cache
+class Set {
+	std::vector<Block> blocks;
+
+
+		
+	// Constructor
+	Set(int blocks, int bytes);
+	// Destructor
+	~Set();
+
+	// Nested class to represent a block
+	class Block {
+		int order;	// Keep track of order based on FIFO or LRU
+		bool dirty;	// Relevant for write-back protocol
+	};
+};
+
+#endif
